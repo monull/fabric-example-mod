24f27432839487f5098edf24b4fb074063ae5d37
BuildTools <unconfigured@null.spigotmc.org>
2021-11-01 22:02:18 +0900

    Santa

---
+++ src/main/java/io/github/monull/santa/SantaMod.java
@@ -0,0 +1,10 @@
+package io.github.monull.santa;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SantaMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
